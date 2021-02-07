26cd89182fe12945a4f7688b2c6c62f526fe6726
MeMyself <1461160060@qq.com>
2021-02-07 20:49:59 +0800

    大致描述

--- app/src/main/java/com/example/chuyan/MainActivity.java
+++ app/src/main/java/com/example/chuyan/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.chuyan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button my_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        my_button=findViewById(R.id.mu_button);
+
+        my_button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+
+            }
+        });
     }
 }
